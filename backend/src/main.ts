f77ef29d52d20e5bb9aa90b0ab2761280da16e09
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-16 07:59:25 +0000

    feat: cors設定とコンテナ外アクセスを許可

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8000);
+  app.enableCors({
+    origin: ['http://frontend:5173', 'http://localhost:5173'],
+    credentials: true,
+  });
+  await app.listen(8000, '0.0.0.0');
 }
 bootstrap();
