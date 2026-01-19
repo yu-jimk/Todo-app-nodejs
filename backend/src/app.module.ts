6584cdf216bb099e184ea29ced5798a102a7ffc0
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-19 13:34:24 +0000

    fix: AppServiceとPrismaServiceのインポートを修正し、拡張子を追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaService } from './prisma/prisma.service.js';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
