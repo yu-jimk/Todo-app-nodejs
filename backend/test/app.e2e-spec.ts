9ad3ad1c4fbba7b41ce8094881cfc090813d542c
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-19 15:18:47 +0000

    feat: importに拡張子を明記

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { App } from 'supertest/types.js';
+import { AppModule } from './../src/app.module.js';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
