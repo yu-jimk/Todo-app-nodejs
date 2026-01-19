2474f5239a0efe61537955a219dfd301b97332c7
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-19 13:33:24 +0000

    feat: PrismaServiceクラスを追加し、データベース接続の初期化と切断を管理

---
+++ backend/src/prisma/prisma.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../generated/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    });
+
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
