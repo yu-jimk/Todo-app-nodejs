63c066e43af8edde957e24c655a256bc3f847294
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-20 10:42:49 +0000

    feat: localhost:8000を追加

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://frontend:5173'],
+    origin: ['http://frontend:5173', 'http://localhost:5173'],
     credentials: true,
   });
   app.useGlobalPipes(
