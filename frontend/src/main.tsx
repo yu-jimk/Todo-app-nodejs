ec200368502ee04cd5abf029bf261b42066989a0
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-20 10:43:49 +0000

    feat: ページ設定の実装

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router.tsx';
+import './shared/styles/index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />);
