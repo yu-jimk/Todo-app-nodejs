56bcf3808b3809300c477a9938a273febe912362
yu-jimk <142887865+yu-jimk@users.noreply.github.com>
2026-01-20 07:40:05 +0000

    feat: 不要なテスト用APIを削除

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,22 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma.service.js';
 
 @Injectable()
 export class AppService {
-  constructor(private prisma: PrismaService) {}
-
   getHello(): string {
     return 'Hello World!';
-  }
-
-  // Todo 例
-  async getTodos() {
-    return await this.prisma.todo.findMany();
-  }
-
-  async createTodo(title: string) {
-    return await this.prisma.todo.create({
-      data: { title },
-    });
   }
 }
